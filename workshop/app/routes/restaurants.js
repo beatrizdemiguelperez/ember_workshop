e3998b3d70f230ff46652f2cd875ed3f27c2e2ef
Beatriz de Miguel <beatriz.demiguel@bbva.com>
2016-11-01 20:00:46 +0100

    > - Change the Restaurant Route to use  resources/restaurants.json as the Model

--- workshop/app/routes/restaurants.js
+++ workshop/app/routes/restaurants.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+const restaurantsUrl = 'https://raw.githubusercontent.com/beatrizdemiguelperez/ember_workshop/master/resources/restaurants.json';
 
 export default Ember.Route.extend({
+
+	model: function() {
+		return Ember.$.getJSON(restaurantsUrl);
+	}
 });
